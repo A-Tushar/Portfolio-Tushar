014024e3eef7bb21d5947350434960d3df537636
A-Tushar <mishurahman2022@gmail.com>
2023-11-25 03:00:14 +0600

    navber
    n

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2023 Your Portfolio. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+// Navbar.js
+
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <>
-    <h1 className='font-sans text-red-300'>Hello World</h1>
-    </>
-  )
-}
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        {/* Brand/Logo */}
+        <a href="/" className="text-white text-lg font-semibold mb-2 md:mb-0 md:mr-4">
+          Your Name
+        </a>
 
-export default Navbar
+        {/* Navigation Links */}
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+          <a href="/" className="text-white">
+            Home
+          </a>
+          <a href="/about" className="text-white">
+            About
+          </a>
+          <a href="/projects" className="text-white">
+            Projects
+          </a>
+          <a href="/contact" className="text-white">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
